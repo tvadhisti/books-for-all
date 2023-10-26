07cfec486367ee2444d51e0fc7b9d84d9b87d161
tvadhisti <tiva.adhisti@ui.ac.id>
2023-10-27 01:30:09 +0700

    add models and modify homepage

--- books_web/homepage/models.py
+++ books_web/homepage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class MasterBooks(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    description = models.TextField()
+    rating = models.IntegerField()
+    
