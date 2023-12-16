84b4b72699dd6e410c62c48f3aa0969a74708841
tvadhisti <tiva.adhisti@ui.ac.id>
2023-12-16 17:50:15 +0700

    add new feature book review

--- books_web/bookpage/urls.py
+++ books_web/bookpage/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:book_id>', views.book)
+    path('<int:book_id>', views.book, name="book_view"),
+    path('review', views.review, name="book_review")
 ]
