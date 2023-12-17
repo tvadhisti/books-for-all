2336427fa939de11a8ff9934c3b207bf82418b36
tvadhisti <tiva.adhisti@ui.ac.id>
2023-12-17 20:49:15 +0700

    Create API feature

---
+++ books_web/api_app/create_api.py
@@ -0,0 +1,59 @@
+from homepage.models import MasterBooks
+from bookpage.models import ReviewItem
+from wishlist.models import WishlistItem
+from question.models import QuestionItem, AnswerItem
+from cart.models import CartItem
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def create_review(request):
+    if request.method == "POST":
+        book_id = request.POST['reviewForm']
+        book = MasterBooks.objects.filter(pk=book_id)
+        ReviewItem.objects.get_or_create(user=request.user, book=book[0], review=request.POST['review'])
+        
+        return JsonResponse({"message":"Review Added"}, status=201)
+    else:
+        return JsonResponse({"message":"Wrong Method"}, status=500)
+
+@csrf_exempt
+def create_wishlist(request):
+    if request.method == "POST":
+        book = MasterBooks.objects.get(id=request.POST['bookid'])
+        # Add the book to the user's wishlist
+        WishlistItem.objects.get_or_create(user=request.user, book=book)
+        return JsonResponse({'message': 'Book added to wishlist'}, status=201)
+    else:
+        return JsonResponse({"message":"Wrong Method"}, status=500)
+
+@csrf_exempt
+def create_cart(request):
+    if request.method == "POST":
+        book = MasterBooks.objects.get(id=request.POST['bookid'])
+        # Add the book to the user's wishlist
+        CartItem.objects.get_or_create(user=request.user, book=book)
+        return JsonResponse({'message': 'Book added to cart'}, status=201)
+    else:
+        return JsonResponse({'message': 'Wrong Method'}, status=500)
+
+@csrf_exempt
+def create_question(request):
+    if request.method == "POST":
+        book_id = request.POST['bookid']
+        book = MasterBooks.objects.filter(pk=book_id)
+        QuestionItem.objects.get_or_create(user=request.user, book=book[0], question=request.POST['question'])
+        return JsonResponse({'message': 'Question added!'}, status=201)
+    else:
+        return JsonResponse({'message': 'Wrong Method'}, status=500)
+
+
+@csrf_exempt
+def create_answer(request):
+    if request.method == "POST":
+        questionid = request.POST['questionid']
+        question = QuestionItem.objects.get(pk=questionid)
+        AnswerItem.objects.get_or_create(user=request.user, question=question, answer=request.POST['answer'])
+        return JsonResponse({'message': 'Answer added'}, status=201)
+    else:
+        return JsonResponse({'message': 'Wrong Method'}, status=500)
--- books_web/api_app/urls.py
+++ books_web/api_app/urls.py
@@ -5,6 +5,11 @@ urlpatterns = [
     # url format
     # add
     # path('add/[name_api]/', create_api.add_to_cart_api, name='add_to_cart_api')
+    path('add/create_review/', create_api.create_review, name='create_review_api'),
+    path('add/create_wishlist/', create_api.create_wishlist, name='create_wishlist_api'),
+    path('add/create_cart/', create_api.create_cart, name='create_cart_api'),
+    path('add/create_question/', create_api.create_question, name='create_question_api'),
+    path('add/create_answer/', create_api.create_answer, name='create_answer_api'),
 
     # url format
     # remove
@@ -13,5 +18,6 @@ urlpatterns = [
     # url format
     # get
     # path('get/get_cart/', get_api.get_cart_api, name='get_cart_api')
-    path('get/get_homepage_book/', get_api.get_homepage_book, name='get_homepage_book_api')
+    path('get/get_homepage_book/', get_api.get_homepage_book, name='get_homepage_book_api'),
+    
 ]
